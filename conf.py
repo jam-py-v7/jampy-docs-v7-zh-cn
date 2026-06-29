0daaeb2b90150a75bd5e200bc2126698a1594969
platipusica <dbabic@gmail.com>
2026-06-29 13:47:34 +0800

    PDF index

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ import shlex
 # ones.
 
 #extensions = ['sphinx_rtd_theme']
-extensions = ['sphinx_copybutton', 'sphinxcontrib.jquery', 'sphinx.ext.linkcode', "sphinx.ext.githubpages", 'sphinx_llms_txt']
+extensions = ['sphinx_copybutton', 'sphinxcontrib.jquery', 'sphinx.ext.linkcode', 'sphinx.ext.githubpages']
 html_baseurl = 'https://jampy-docs.readthedocs.io/projects/V7/zh-cn/latest'
 
 html_context = {
